da6dd76b885ab9215fcd6ff9172ee20675a03061
David <david.rbacelar@udc.es>
2022-04-04 00:14:05 +0200

    Update: manageVolume script in sxhkd

--- simple/.config/sxhkd/scripts/manageVolume.sh
+++ simple/.config/sxhkd/scripts/manageVolume.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+getDefaultSinkName() {
+  pacmd stat | awk -F": " '/^Default sink name: /{print $2}'
+}
+
+getDefaultSinkVolume() {
+	pacmd list-sinks |
+		awk '/^\s+name: /{indefault = $2 == "<'$(getDefaultSinkName)'>"}
+			/^\s+volume: / && indefault {print $5; exit}' |
+		awk -F "%" '{print $1}'
+}
+
+getDefaultSinkMute() {
+	pacmd list-sinks |
+		awk '/^\s+name: /{indefault = $2 == "<'$(getDefaultSinkName)'>"}
+			/^\s+muted: / && indefault {print $2; exit}' |
+		awk -F "%" '{print $1}'
+}
+
+setDefaultSinkVol() {
+  pactl set-sink-volume $(getDefaultSinkName) $1
+}
+
 if [ "$1" == "toggleMute"  ]; then
 	pactl set-sink-mute @DEFAULT_SINK@ toggle
-	if pactl get-sink-mute @DEFAULT_SINK@ | grep -i "yes" &>/dev/null; then
+	if [ "$(getDefaultSinkMute)" == "yes" ]; then
 		dunstify -a System -t 1000 -h string:x-dunst-stack-tag:volume "Audio Muted"
 	else
 		dunstify -a System -t 1000 -h string:x-dunst-stack-tag:volume "Audio Unmuted"
@@ -11,13 +33,13 @@ if [ "$1" == "toggleMute"  ]; then
 fi
 
 VOLUME_STEP=2
-if pactl get-default-sink | grep -i "bluez_sink" &>/dev/null; then
+if getDefaultSinkName | grep -i "bluez_sink" &>/dev/null; then
 	VOLUME_STEP=1
 fi
 
-PREV_VOL=$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+PREV_VOL=$(getDefaultSinkVolume)
 if [[ "$1" == "-" || $PREV_VOL -le $((100 - $VOLUME_STEP)) ]]; then
-	pactl set-sink-volume @DEFAULT_SINK@ $1$VOLUME_STEP%
+	setDefaultSinkVol "$1$VOLUME_STEP%"
 fi
-CURRENT_VOL=$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+CURRENT_VOL=$(getDefaultSinkVolume)
 dunstify -a System -t 1000 -h string:x-dunst-stack-tag:volume -h int:value:$CURRENT_VOL "Volume: $CURRENT_VOL%"
