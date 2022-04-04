7ef575cf8deb526dbfb11b718620e818b4f39d45
David <david.rbacelar@udc.es>
2022-04-05 01:20:32 +0200

    Add: new spotify theme in wip

--- minimal/gtheme/post-scripts/spotify.sh
+++ minimal/gtheme/post-scripts/spotify.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Disable previous extensions
+EXTENSIONS=($(spicetify config extensions))
+for E in ${EXTENSIONS[@]}; do
+	spicetify config extensions $E-
+done
+
 spicetify config current_theme Sleek color_scheme Custom
 spicetify update
-exit 0
+spicetify update -e
--- wip/gtheme/post-scripts/spotify.sh
+++ wip/gtheme/post-scripts/spotify.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-spicetify config current_theme Sleek color_scheme Custom
+# Disable previous extensions
+EXTENSIONS=($(spicetify config extensions))
+for E in ${EXTENSIONS[@]}; do
+	spicetify config extensions $E-
+done
+
+spicetify config extensions dribbblish.js
+spicetify config current_theme Dribbblish color_scheme Custom
+spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
 spicetify update
-exit 0
+spicetify update -e
