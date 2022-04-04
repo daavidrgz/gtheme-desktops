7a61253b367e0fa5f1a1115379c5613ce1c9e471
David <david.rbacelar@udc.es>
2022-04-04 18:00:31 +0200

    Update: manageVolume script in japan and minimal

--- minimal/.config/sxhkd/scripts/manageVolume.sh
+++ minimal/.config/sxhkd/scripts/manageVolume.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
-VOLUME_STEP=2
+getDefaultSinkName() {
+  pacmd stat | awk -F": " '/^Default sink name: /{print $2}'
+}
 
-if pactl get-default-sink | grep -i "bluez_sink" &>/dev/null; then
+getDefaultSinkVolume() {
+	pacmd list-sinks | tr -d '\n' | grep -Pozi "$(getDefaultSinkName).*volume.*%" | grep -Poz "(?<=/)[ ]*(\d+)(?=%)" | awk '{print $1}'
+}
+
+getDefaultSinkMute() {
+	pacmd list-sinks | tr -d '\n' | grep -Pozi "$(getDefaultSinkName).*(muted:[ ]*(yes|no))" | grep -Pozi "(?<=muted:)(?:.*)(yes|no)" | tr -d '\0 '
+}
+
+if [ "$1" == "--toggle"  ]; then
+	pactl set-sink-mute @DEFAULT_SINK@ toggle
+	if [ "$(getDefaultSinkMute)" == "yes" ]; then
+		dunstify -a System -t 1000 -h string:x-dunst-stack-tag:volume "Audio Muted"
+	else
+		dunstify -a System -t 1000 -h string:x-dunst-stack-tag:volume "Audio Unmuted"
+	fi
+	exit 0
+fi
+
+VOLUME_STEP=2
+if getDefaultSinkName | grep -i "bluez_sink" &>/dev/null; then
 	VOLUME_STEP=1
 fi
 
-pamixer $1 $VOLUME_STEP \
-&& dunstify -a System -t 1000 -h string:x-dunst-stack-tag:volume -h int:value:$(pamixer --get-volume) "Volume: $(pamixer --get-volume)%"
+if [ "$1" == "--decrease" ]; then
+	VOLUME_STEP=-$VOLUME_STEP
+fi
+
+PREV_VOL=$(getDefaultSinkVolume)
+if [[ "$1" == "--decrease" || $PREV_VOL -le $((100 - $VOLUME_STEP)) ]]; then
+  pactl set-sink-volume @DEFAULT_SINK@ "$(($PREV_VOL + $VOLUME_STEP))%"
+fi
+CURRENT_VOL=$(getDefaultSinkVolume)
+dunstify -a System -t 1000 -h string:x-dunst-stack-tag:volume -h int:value:$CURRENT_VOL "Volume: $CURRENT_VOL%"
